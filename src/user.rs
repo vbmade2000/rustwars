d8a17f1d4a74f2b5bf5dd7c2e3834f3038c4d932
Malhar Vora <malhar.vora@seagate.com>
2020-01-17 18:28:49 +0530

    Add User struct
    User struct represents a single user. For now it contains few fields related
    to user. More fields to be added in that.

--- src/user.rs
+++ src/user.rs
@@ -3,4 +3,13 @@ pub mod user {
     pub fn print_mac() {
         println!("This is from mod")
     }
+
+    #[derive(Debug)]
+    pub struct User {
+        pub username: String,
+        pub name: String,
+        pub clan: String,
+        pub leader_board_position: String,
+        pub skills: String
+    }
 }
